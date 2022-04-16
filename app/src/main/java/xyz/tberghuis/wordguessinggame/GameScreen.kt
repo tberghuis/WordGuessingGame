ef3e9f195e38e4d5bab8914aeaf2d59e9ab9c8aa
Thomas Berghuis <thomas.berghuis@gmail.com>
2022-04-16 15:08:16 +1000

    .

--- app/src/main/java/xyz/tberghuis/wordguessinggame/GameScreen.kt
+++ app/src/main/java/xyz/tberghuis/wordguessinggame/GameScreen.kt
@@ -1,10 +1,20 @@
 package xyz.tberghuis.wordguessinggame
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 
 
 @Composable
 fun GameScreen() {
-  Text("hello game screen")
+
+
+  val viewModel: WordleViewModel = hiltViewModel()
+
+
+  Column {
+    Text("hello game screen ${viewModel.hellovm}")
+  }
+
 }
