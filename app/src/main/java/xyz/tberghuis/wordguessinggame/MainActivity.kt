6b83cc50984460ebfe683386b9f51eef98d54276
Thomas Berghuis <thomas.berghuis@gmail.com>
2022-04-17 04:53:06 +1000

    .

--- app/src/main/java/xyz/tberghuis/wordguessinggame/MainActivity.kt
+++ app/src/main/java/xyz/tberghuis/wordguessinggame/MainActivity.kt
@@ -1,25 +1,15 @@
 package xyz.tberghuis.wordguessinggame
 
 import android.os.Bundle
-import android.view.KeyEvent
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.hilt.navigation.compose.hiltViewModel
-import xyz.tberghuis.wordguessinggame.tmp.KeyboardDemo
 import xyz.tberghuis.wordguessinggame.ui.theme.WordGuessingGameTheme
-import xyz.tberghuis.wordguessinggame.util.logd
 
 class MainActivity : ComponentActivity() {
-
 
 //  val viewModel by viewModels<WordleViewModel>()
 
@@ -36,10 +26,8 @@ class MainActivity : ComponentActivity() {
     }
   }
 
-
 //  override fun onKeyUp(keyCode: Int, event: KeyEvent): Boolean {
 //    logd("KeyEvent $event")
 //    return true
 //  }
 }
-
