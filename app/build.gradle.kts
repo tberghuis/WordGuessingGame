170462f824f7dd351d37782acc8826f66fcc9a59
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-10-20 11:28:33 +1100

    .

---
+++ app/build.gradle.kts
@@ -0,0 +1,67 @@
+plugins {
+  alias(libs.plugins.android.application)
+  alias(libs.plugins.kotlin.android)
+}
+
+android {
+  compileSdk = 33
+
+  defaultConfig {
+    applicationId = "xyz.tberghuis.wordguessinggame"
+    minSdk = 21
+    targetSdk = 33
+    versionCode = 5
+    versionName = "1.3.0"
+
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    vectorDrawables {
+      useSupportLibrary = true
+    }
+  }
+
+  buildTypes {
+    release {
+      isMinifyEnabled = false
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+    }
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+  }
+  buildFeatures {
+    compose = true
+  }
+  composeOptions {
+    kotlinCompilerExtensionVersion = "1.4.3"
+  }
+  packagingOptions {
+    resources {
+      excludes += "/META-INF/{AL2.0,LGPL2.1}"
+    }
+  }
+  namespace = "xyz.tberghuis.wordguessinggame"
+}
+
+kotlin {
+  jvmToolchain(17)
+}
+
+dependencies {
+
+  val composeBom = platform("androidx.compose:compose-bom:2023.01.00")
+  implementation(composeBom)
+  androidTestImplementation(composeBom)
+  implementation("androidx.compose.material:material")
+  implementation("androidx.compose.ui:ui")
+  implementation("androidx.compose.ui:ui-tooling-preview")
+  debugImplementation("androidx.compose.ui:ui-tooling")
+  androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+  debugImplementation("androidx.compose.ui:ui-test-manifest")
+  implementation("androidx.compose.material:material-icons-extended")
+
+  implementation("androidx.navigation:navigation-compose:2.4.2")
+
+
+  implementation("androidx.datastore:datastore-preferences:1.0.0")
+}
