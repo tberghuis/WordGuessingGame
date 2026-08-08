f6baa30b0ce093f8483f176bec071fa4e15fa1f3
Thomas Berghuis <thomas.berghuis@gmail.com>
2026-08-08 11:56:39 +1000

    .

--- app/src/main/java/xyz/tberghuis/wordguessinggame/GameScreen.kt
+++ app/src/main/java/xyz/tberghuis/wordguessinggame/GameScreen.kt
@@ -2,8 +2,8 @@ package xyz.tberghuis.wordguessinggame
 
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
